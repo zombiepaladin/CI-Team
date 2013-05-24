b3482a720597b74830c8bef2757195934741c44e
Borzen <Borzen00@yahoo.com>
2013-05-24 10:49:40 -0500

    MetaData Added and now is official release of pre-Alpha0.1

---
+++ SF/Assets/Farmer/DriveVehicle.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A MonoBehaviour that is designed to have the farmer get in the vehicle.
+/// </summary>
+public class DriveVehicle : MonoBehaviour {
+	/// <summary>
+	/// The player object.
+	/// </summary>
+	public GameObject playerObj;
+	/// <summary>
+	/// The player camera.
+	/// </summary>
+	public Camera playerCamera;
+	/// <summary>
+	/// Colider that is the trigger to see if the player is in there.
+	/// </summary>
+	public Transform doorTriggerLeft;
+	/// <summary>
+	/// Is the player visible (Default is false).
+	/// </summary>
+	public bool isPlayerVisible;
+	/// <summary>
+	/// Is the key press (Default is false).
+	/// </summary>
+	bool isKeyPressed = false;
+	
+	/* 
+	 * Update is called once per frame.
+	 * 
+	 * this sees if the E key is press when the player is visible and is so disables the moving code so the player cannot move.
+	 */
+	void Update () {
+		if(Input.GetKeyUp (KeyCode.E) && isPlayerVisible){
+			playerCamera.gameObject.GetComponent<MouseLook>().enabled = false;
+			playerObj.GetComponent<MouseLook>().enabled = false;
+			playerObj.SendMessage("enableYourself",false);
+			isKeyPressed = true;
+		}
+	}
+	/*
+	 * This is what handels the GUI.
+	 * 
+	 * The if statment draws a box with buttons in it if the player is visible
+	 * Each one loads a different scene that handles how the tractor is driven.
+	 * The cancel button renables movement and makes iskeyPressed false.
+	 * 
+	 */
+	void OnGUI(){
+		if(isKeyPressed){
+			GUI.Box(new Rect(600,300,800,100),"What do you want to do?");
+			if(GUI.Button(new Rect(600,325,150,75), "AI tractor")){
+				Application.LoadLevel("AI Tractor");
+			}
+			if(GUI.Button(new Rect(750,325,150,75), "First Person Tractor")){
+				Application.LoadLevel("FPTractorEnvierment");
+			}
+			if(GUI.Button(new Rect(900,325,150,75), "3rd perosn Tractor")){
+				Application.LoadLevel("TractorController");
+			}
+			if(GUI.Button(new Rect(1050,325,150,75), "Cancel")){
+				playerCamera.gameObject.GetComponent<MouseLook>().enabled = true;
+				playerObj.GetComponent<MouseLook>().enabled = true;
+				playerObj.SendMessage("enableYourself",true);
+				isKeyPressed = false;
+			}
+		}
+	}
+	
+	/*
+	 * a on trigger even to see if the door has been entered by the player.
+	 */
+	void OnTriggerEnter(Collider Player){
+		isPlayerVisible = true;
+	}
+	/*
+	 * a trigger to see if the player has exited the cube.
+	 */
+	void OnTriggerExit(Collider Player){
+		isPlayerVisible = false;
+	}
+}
