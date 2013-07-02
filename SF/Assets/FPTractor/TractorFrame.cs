3e7550028cc9e292863b094f016c620f74d9c3a4
Borzen <Borzen00@gmail.com>
2013-07-02 11:57:28 -0500

    instanced frame turns now

--- SF/Assets/FPTractor/TractorFrame.cs
+++ SF/Assets/FPTractor/TractorFrame.cs
@@ -15,12 +15,14 @@ public class TractorFrame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A))&& frame.GetComponent<Transform>().rotation.z < 90){
-			instancedFrame.GetComponent<Transform>().rotation = new Quaternion(instancedFrame.GetComponent<Transform>().rotation.x+(.001f),instancedFrame.GetComponent<Transform>().rotation.y,instancedFrame.GetComponent<Transform>().rotation.z,instancedFrame.GetComponent<Transform>().rotation.w);
+		if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) && frame.GetComponent<Transform>().rotation.z < 90){
+			instancedFrame.GetComponent<Transform>().rotation = new Quaternion(instancedFrame.GetComponent<Transform>().rotation.x,instancedFrame.GetComponent<Transform>().rotation.y,instancedFrame.GetComponent<Transform>().rotation.z+.001f,instancedFrame.GetComponent<Transform>().rotation.w);
 			//frame.GetComponent<Transform>().rotation = (new Quaternion(frame.GetComponent<Transform>().rotation.x,frame.GetComponent<Transform>().rotation.y,frame.GetComponent<Transform>().rotation.z,frame.GetComponent<Transform>().rotation.w))*(new Quaternion(frame.GetComponent<Transform>().rotation.x,frame.GetComponent<Transform>().rotation.y,frame.GetComponent<Transform>().rotation.z+1f,frame.GetComponent<Transform>().rotation.w));
 		}
-		else if((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))&& frame.GetComponent<Transform>().rotation.z >= 0){
-			instancedFrame.GetComponent<Transform>().rotation = new Quaternion(instancedFrame.GetComponent<Transform>().rotation.x-(.001f),instancedFrame.GetComponent<Transform>().rotation.y,instancedFrame.GetComponent<Transform>().rotation.z,instancedFrame.GetComponent<Transform>().rotation.w);
+	
+		
+		if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))&& frame.GetComponent<Transform>().rotation.z >= 0){
+			instancedFrame.GetComponent<Transform>().rotation = new Quaternion(instancedFrame.GetComponent<Transform>().rotation.x,instancedFrame.GetComponent<Transform>().rotation.y,instancedFrame.GetComponent<Transform>().rotation.z-(.001f),instancedFrame.GetComponent<Transform>().rotation.w);
 			//frame.GetComponent<Transform>().rotation = (new Quaternion(frame.GetComponent<Transform>().rotation.x,frame.GetComponent<Transform>().rotation.y,frame.GetComponent<Transform>().rotation.z,frame.GetComponent<Transform>().rotation.w))*(new Quaternion(frame.GetComponent<Transform>().rotation.x,frame.GetComponent<Transform>().rotation.y,frame.GetComponent<Transform>().rotation.z-1f,frame.GetComponent<Transform>().rotation.w));
 		}
 		instancedFrame.GetComponent<Transform>().position = new Vector3(sp.GetComponent<Transform>().position.x+13.0f,sp.GetComponent<Transform>().position.y,sp.GetComponent<Transform>().position.z+8.75f);
