c5a5340569f2b6d5b47d5523fa8581097805ed8b
Borzen <Borzen00@gmail.com>
2013-07-03 09:13:19 -0500

    Added documentation to the crop script.

--- SF/Assets/World Scripts/Crop.cs
+++ SF/Assets/World Scripts/Crop.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// A class for crops.
+/// </summary>
 public class Crop{
+	
+	/// <summary>
+	/// Crop type.
+	/// </summary>
 	enum CropType{
 		corn = 0,
 		wheat = 1,
@@ -10,22 +17,46 @@ public class Crop{
 		grass,
 	};
 	
+	/// <summary>
+	/// What the game object is for the crop.
+	/// </summary>
 	GameObject cr;
 	
+	/// <summary>
+	/// The crop type.
+	/// </summary>
 	CropType ct = 0;
 	
+	/// <summary>
+	/// The panted crops stored as game objects.
+	/// </summary>
 	List<GameObject> pantedCrops = new List<GameObject>();
 	
+	/// <summary>
+	/// The positions of the crops stored as vector3.
+	/// </summary>
 	List<Vector3> positions = new List<Vector3>();
 	
 
 	public Crop(){
 	}
 	
+	/// <summary>
+	/// Sets the type.
+	/// </summary>
+	/// <param name='t'>
+	/// the type as an int.
+	/// </param>
 	public void setType(int t){
 		ct = (CropType)t;
 	}
 	
+	/// <summary>
+	/// Plants the crop.
+	/// </summary>
+	/// <param name='pos'>
+	/// Position as a vector 3.
+	/// </param>
 	public void PlantCrop(Vector3 pos){
 		switch(ct){
 		case CropType.corn:
@@ -50,6 +81,15 @@ public class Crop{
 		}
 	}
 	
+	/// <summary>
+	/// Plants the crop.
+	/// </summary>
+	/// <param name='pos'>
+	/// Position of the crop.
+	/// </param>
+	/// <param name='type'>
+	/// Type of the crop.
+	/// </param>
 	public void PlantCrop(Vector3 pos, int type){
 		switch(type){
 		case 0:
@@ -74,17 +114,42 @@ public class Crop{
 		}
 	}
 	
+	/// <summary>
+	/// Gets the planted crops game object.
+	/// </summary>
+	/// <returns>
+	/// The planted crops.
+	/// </returns>
 	public List<GameObject> getPlantedCrops(){
 		return pantedCrops;
 	}
 	
+	/// <summary>
+	/// Gets the positions in the vector3 array.
+	/// </summary>
+	/// <returns>
+	/// The positions.
+	/// </returns>
 	public List<Vector3> getPositions(){
 		return positions;	
 	}
+	
+	/// <summary>
+	/// Gets the crop.
+	/// </summary>
+	/// <returns>
+	/// The crop as a string.
+	/// </returns>
 	public string getCrop(){
 		return ct.ToString();
 	}
 	
+	/// <summary>
+	/// Gets the position as vec4.
+	/// </summary>
+	/// <returns>
+	/// The position as vec4.
+	/// </returns>
 	public List<Vector4> getPosAsVec4(){
 		List<Vector4> v4list = new List<Vector4>();
 		
@@ -95,6 +160,15 @@ public class Crop{
 		return v4list;
 	}
 	
+	/// <summary>
+	/// Lasts the plant.
+	/// </summary>
+	/// <returns>
+	/// The plant.
+	/// </returns>
+	/// <param name='pos'>
+	/// If set to <c>true</c> position.
+	/// </param>
 	public bool lastPlant(Vector3 pos){
 		if(pos.z >= positions[positions.Count-1].z){
 			return true;
@@ -104,6 +178,12 @@ public class Crop{
 		}
 	}
 	
+	/// <summary>
+	/// Sets the field.
+	/// </summary>
+	/// <param name='crops'>
+	/// Crops in a vector4 format loaded from file.
+	/// </param>
 	public void setField(List<Vector4> crops){
 		bool typeSet = false;
 		foreach(Vector4 v in crops){
